27444039bea8c13358e91de89850a9c6e4949971
figredos <matheusmfellet@gmail.com>
2023-11-24 19:35:21 -0300

    main file

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -5,12 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(222, 255, 0, 0),
-        body: GradientContainer(
-            color_1: Color.fromARGB(255, 255, 17, 0),
-            color_2: Color.fromARGB(255, 253, 39, 39),
-            startAlignment: Alignment.topCenter,
-            endAlignment: Alignment.bottomCenter),
+        body: GradientContainer.red(),
       ),
     ),
   );
