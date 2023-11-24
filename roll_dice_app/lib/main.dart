b691bae25d2c32b6c8277d02ced791fad0805725
figredos <matheusmfellet@gmail.com>
2023-11-23 23:49:59 -0300

    Building widget tree

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello, World!"),
-        ),
+        backgroundColor: Color.fromARGB(222, 255, 0, 0),
+        body: GradientContainer(
+            color_1: Color.fromARGB(255, 255, 17, 0),
+            color_2: Color.fromARGB(255, 253, 39, 39),
+            startAlignment: Alignment.topCenter,
+            endAlignment: Alignment.bottomCenter),
       ),
     ),
   );
