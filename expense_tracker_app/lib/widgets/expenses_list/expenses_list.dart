3bee3cb0b968cea37b2d9358d491ad80084f947f
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-12-23 21:56:54 -0300

    Expense list

---
+++ expense_tracker_app/lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,21 @@
+import 'package:expense_tracker_app/widgets/expenses_list/expenses_item.dart';
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker_app/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => ExpensesItem(expense: expenses[index]),
+    );
+  }
+}
