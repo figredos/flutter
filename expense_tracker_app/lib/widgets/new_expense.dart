903d4bb1a72ad936f2e0f4b47f4c61ed80d2041d
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-12-26 12:37:13 -0300

    Adding Dropdown button for category

--- expense_tracker_app/lib/widgets/new_expense.dart
+++ expense_tracker_app/lib/widgets/new_expense.dart
@@ -15,6 +15,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -87,15 +88,33 @@ class _NewExpenseState extends State<NewExpense> {
             height: 16,
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) return;
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
+              TextButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
                 child: const Text('Cancel'),
               ),
-              const Spacer(),
               ElevatedButton(
                 onPressed: () {
                   print(_amountController.text);
