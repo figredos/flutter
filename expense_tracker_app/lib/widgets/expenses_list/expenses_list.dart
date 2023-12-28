9d8e2f7359e199da808477830ef9d88e6e7acc45
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-12-28 00:14:44 -0300

    Dismissing expenses from list

--- expense_tracker_app/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker_app/lib/widgets/expenses_list/expenses_list.dart
@@ -7,15 +7,29 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onDismissedExpense,
   });
 
+  final void Function(Expense expense) onDismissedExpense;
   final List<Expense> expenses;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => ExpensesItem(expense: expenses[index]),
+      itemBuilder: (context, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(.75),
+          margin: EdgeInsets.symmetric(horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+        ),
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onDismissedExpense(
+            expenses[index],
+          );
+        },
+        child: ExpensesItem(expense: expenses[index]),
+      ),
     );
   }
 }
