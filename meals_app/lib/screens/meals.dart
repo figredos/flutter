e55bb23a6e91aa42df076806100b1031199284b5
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-01-09 17:21:50 -0300

    Navigation to meal details

--- meals_app/lib/screens/meals.dart
+++ meals_app/lib/screens/meals.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/meal_item.dart';
+import 'package:meals_app/screens/meal_details.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -12,6 +13,16 @@ class MealsScreen extends StatelessWidget {
 
   final String title;
   final List<Meal> meals;
+
+  void selectedMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +57,9 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal: meals[index])
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index], onSelectMeal:(meal){
+          selectedMeal(context, meal);
+        }, )
       );
     }
 
