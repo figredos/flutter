e8cf2443becf1255b20acd7b916e9bd5fd884b07
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-01-09 15:32:12 -0300

    Showing titles from dummy meals

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -3,18 +3,25 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectedCategory(BuildContext context) {
+  void _selectedCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'Some Title',
-          meals: [],
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -39,7 +46,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
                 category: category,
                 onSelectedCategory: () {
-                  _selectedCategory(context);
+                  _selectedCategory(context, category);
                 })
         ],
       ),
