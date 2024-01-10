89b42ff72cb565346cb677f557723e99c0ffad4c
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-01-10 13:28:15 -0300

    Removing hard coded title

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -29,11 +29,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(16),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -49,7 +45,6 @@ class CategoriesScreen extends StatelessWidget {
                   _selectedCategory(context, category);
                 })
         ],
-      ),
-    );
+      );
   }
 }
