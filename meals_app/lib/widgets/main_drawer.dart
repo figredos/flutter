6f5a4c217225efcd8f191510df4111f85f890746
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-01-12 00:03:04 -0300

    Adding pages ListTiles

--- meals_app/lib/widgets/main_drawer.dart
+++ meals_app/lib/widgets/main_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({super.key, required this.onSelectScreen,});
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -9,32 +11,62 @@ class MainDrawer extends StatelessWidget {
       child: Column(
         children: [
           DrawerHeader(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Theme.of(context).colorScheme.primaryContainer,
-                  Theme.of(context)
-                      .colorScheme
-                      .primaryContainer
-                      .withOpacity(0.8),
-                ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-              ),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.fastfood,
-                    size: 48,
-                    color: Theme.of(context).colorScheme.primary,
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Theme.of(context).colorScheme.primaryContainer,
+                Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8),
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.fastfood,
+                  size: 48,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: 18),
+                Text(
+                  'Cooking up!',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.restaurant,
+              size: 26,
+            ),
+            title: Text(
+              'Meals',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24,
                   ),
-                  const SizedBox(width: 18),
-                  Text(
-                    'Cooking up!',
-                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                  )
-                ],
-              )),
+            ),
+            onTap: () {
+              onSelectScreen('meals');
+            },
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.settings,
+              size: 26,
+            ),
+            title: Text(
+              'Filters',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24,
+                  ),
+            ),
+            onTap: () {
+              onSelectScreen('filters');
+            },
+          ),
         ],
       ),
     );
