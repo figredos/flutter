13a2a93a18260efd389e96b9b3f1ee8684d232ce
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-01-22 23:02:13 -0300

    Fixing filtered list

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -44,7 +44,9 @@ class _CategoriesScreenState extends State<CategoriesScreen>
   }
 
   void _selectedCategory(BuildContext context, Category category) {
-    final filteredMeals = widget.availableMeals;
+    final filteredMeals = widget.availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
 
     Navigator.of(context).push(
       MaterialPageRoute(
