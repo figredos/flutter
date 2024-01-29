1b342ce4cb6d311561d0250e37059947eccc3f72
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-01-29 19:30:12 -0300

    Renaming directory

--- shopping_list_app/lib/main.dart
+++ shopping_list_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart'; 
 
-import 'package:shopping_list_app/widgets.dart/grocery_list.dart';
+import 'package:shopping_list_app/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
