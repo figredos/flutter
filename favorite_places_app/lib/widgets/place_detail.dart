9dd8f39d7c9da0134ad7ab3625a5fedb9bdbb2ea
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-02-16 16:49:08 -0300

    Changing main widget

--- favorite_places_app/lib/widgets/place_detail.dart
+++ favorite_places_app/lib/widgets/place_detail.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:favorite_places_app/models/place.dart';
 
@@ -17,13 +16,21 @@ class PlaceDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(place.title),
       ),
-      body: Center(
-        child: Text(
-          place.title,
-          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-        ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Text(
+            place.title,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
       ),
     );
   }
