82a1c4093eb607916022e2f3f8c96df69f5bb192
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-02-19 21:22:28 -0300

    Configurations for the google_maps_flutter package

--- favorite_places_app/ios/Runner/AppDelegate.swift
+++ favorite_places_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDYytfH-eoC_JQ0e2q-HUkPdmtPuhAMI2w")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
