01b11883794c00b792dec081cfce2ea5e891cfe8
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2024-02-22 18:08:20 -0300

    Adding Firebase compatibility

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-import 'package:chat_app/screens/auth.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+import 'package:chat_app/screens/splash.dart';
+import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -12,13 +22,25 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'FlutterChat',
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const AuthScreen()
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
